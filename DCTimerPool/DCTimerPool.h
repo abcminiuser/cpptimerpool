f7c03ef7b5997c2a7ba82e343e7eac984d60bc16
Dean Camera <dean@fourwalledcubicle.com>
2018-11-02 22:12:28 +1100

    Clean up type aliases.

--- DCTimerPool/DCTimerPool.h
+++ DCTimerPool/DCTimerPool.h
@@ -8,65 +8,70 @@
 #include <forward_list>
 
 
-class TimerPool : public std::enable_shared_from_this<TimerPool>
+class TimerPool
+	: public std::enable_shared_from_this<TimerPool>
 {
 public:
 	class Timer;
 
 	using Clock = std::chrono::steady_clock;
-	using Pool = std::shared_ptr<TimerPool>;
-	using TimerHandle = std::weak_ptr<Timer>;
+	using PoolHandle = std::shared_ptr<TimerPool>;
+	using WeakTimerHandle = std::weak_ptr<Timer>;
 
-	static Pool									CreatePool();
+	static PoolHandle				CreatePool();
 
-	virtual										~TimerPool();
+	virtual							~TimerPool();
 
-	void										stop();
-	bool										running() const { return m_running; }
+	void							stop();
+	bool							running() const { return m_running; }
 
-	void										wake();
+	void							wake();
 
-	TimerHandle									createTimer();
-	void										deleteTimer(TimerHandle handle);
+	WeakTimerHandle					createTimer();
+	void							deleteTimer(WeakTimerHandle handle);
 
 protected:
-	using StrongTimerHandle = std::shared_ptr<Timer>;
+	using TimerHandle = std::shared_ptr<Timer>;
 
-	explicit									TimerPool();
+	explicit						TimerPool();
 
-	void										run();
-	
+	void							run();
+
 private:
-	std::mutex									m_mutex;
-	std::condition_variable						m_cond;
-	std::forward_list<StrongTimerHandle>		m_timers;
-	bool										m_running;
-	std::thread									m_thread;
+	mutable std::mutex				m_mutex;
+	std::condition_variable			m_cond;
+	std::forward_list<TimerHandle>	m_timers;
+	bool							m_running;
+	std::thread						m_thread;
 };
 
 class TimerPool::Timer
 {
 public:
-	explicit									Timer(TimerPool::Pool parentPool);
+	using Clock = TimerPool::Clock;
+	using Callback = std::function<void(void)>;
+	using PoolHandle = TimerPool::PoolHandle;
+
+	explicit									Timer(PoolHandle parentPool);
 	virtual										~Timer() = default;
 
-	void										setCallback(std::function<void(void)>&& callback);
+	void										setCallback(Callback&& callback);
 	void										setInterval(std::chrono::milliseconds ms);
 	void										setRepeated(bool repeated);
 
 	void										start();
 	void										stop();
 
-	TimerPool::Clock::time_point				nextExpiry() const;
-	TimerPool::Clock::time_point				fire();
+	Clock::time_point							nextExpiry() const;
+	Clock::time_point							fire();
 
 private:
-	TimerPool::Pool								m_parent;
 	mutable std::mutex							m_mutex;
-	TimerPool::Clock::time_point				m_nextExpiry;
+	PoolHandle									m_parent;
+	Clock::time_point							m_nextExpiry;
 
 	bool										m_running;
-	std::function<void(void)>					m_callback;
+	Callback									m_callback;
 	std::chrono::milliseconds					m_interval;
 	bool										m_repeated;
 };
