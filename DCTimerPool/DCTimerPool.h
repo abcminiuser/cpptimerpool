78187b1e41ec2670468e2ddcd06d8cf4f29b618f
Dean Camera <dean@fourwalledcubicle.com>
2018-11-02 21:57:58 +1100

    Make sure timers maintain shared references to their parent pools.

--- DCTimerPool/DCTimerPool.h
+++ DCTimerPool/DCTimerPool.h
@@ -2,63 +2,66 @@
 
 #include <thread>
 #include <mutex>
-#include <atomic>
 #include <condition_variable>
 #include <functional>
 #include <memory>
 #include <forward_list>
 
 
-class TimerPool
+class TimerPool : public std::enable_shared_from_this<TimerPool>
 {
 public:
 	class Timer;
 
 	using Clock = std::chrono::steady_clock;
+	using Pool = std::shared_ptr<TimerPool>;
 	using TimerHandle = std::weak_ptr<Timer>;
 
-	explicit									TimerPool();
+	static Pool									CreatePool();
+
 	virtual										~TimerPool();
 
 	void										stop();
-	bool										running() const { return m_run; }
+	bool										running() const { return m_running; }
 
 	void										wake();
 
 	TimerHandle									createTimer();
 	void										deleteTimer(TimerHandle handle);
 
-private:
+protected:
 	using StrongTimerHandle = std::shared_ptr<Timer>;
+
+	explicit									TimerPool();
 
 	void										run();
 	
 private:
 	std::mutex									m_mutex;
 	std::condition_variable						m_cond;
-	std::thread									m_thread;
 	std::forward_list<StrongTimerHandle>		m_timers;
-	bool										m_run;
+	bool										m_running;
+	std::thread									m_thread;
 };
 
 class TimerPool::Timer
 {
 public:
-	explicit									Timer(TimerPool& parentPool);
+	explicit									Timer(TimerPool::Pool parentPool);
 	virtual										~Timer() = default;
 
 	void										setCallback(std::function<void(void)>&& callback);
 	void										setInterval(std::chrono::milliseconds ms);
 	void										setRepeated(bool repeated);
+
 	void										start();
 	void										stop();
 
-	TimerPool&									parent() const { return m_parent; }
 	TimerPool::Clock::time_point				nextExpiry() const;
 	TimerPool::Clock::time_point				fire();
 
 private:
-	TimerPool&									m_parent;
+	TimerPool::Pool								m_parent;
 	mutable std::mutex							m_mutex;
 	TimerPool::Clock::time_point				m_nextExpiry;
 
--- DCTimerPool/Test.cpp
+++ DCTimerPool/Test.cpp
@@ -1,17 +1,23 @@
-#include "pch.h"
-
 #include "DCTimerPool.h"
 
 
 int main()
 {
-	TimerPool tp;
+	auto pool = TimerPool::CreatePool();
 
-	auto handle = tp.createTimer();
+	auto handle = pool->createTimer();
 	if (auto t = handle.lock())
 	{
 		t->setCallback([]() { printf("TICK!\n"); });
 		t->setInterval(std::chrono::milliseconds(1000));
+		t->start();
+	}
+
+	auto handle2 = pool->createTimer();
+	if (auto t = handle2.lock())
+	{
+		t->setCallback([]() { printf("TOCK!\n"); });
+		t->setInterval(std::chrono::milliseconds(100));
 		t->start();
 	}
 
